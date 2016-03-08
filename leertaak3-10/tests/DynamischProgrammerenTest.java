7cd31635f13256eb227964c4fe7ac164eacc5c37
Roelof Kallenkoot <roelof000@gmail.com>
2016-03-08 22:15:25 +0100

    goddomme

---
+++ leertaak3-10/tests/DynamischProgrammerenTest.java
@@ -0,0 +1,47 @@
+
+import dynProg.Solver;
+import dynProg.solvers.BottomUpSolver;
+import org.junit.*;
+
+
+import dynProg.solvers.RecursiveSolver;
+import static org.junit.Assert.*;
+
+
+public class DynamischProgrammerenTest {
+	int[] numbers = null;
+	int sum;
+	Solver solver;
+	
+	@Test
+	public void testRecursive(){
+		//solver = new RecursiveSolver();
+		//doTest();
+		
+	}
+	
+	@Test
+	public void testBottomUp(){
+		solver = new BottomUpSolver();
+		doTest();
+	}
+	
+	@Test
+	public void testTopDown(){
+		//solver = new TopDownSolver();
+		//doTest();
+	}
+	
+	private void doTest(){
+		// 3+5+9=17
+		assertTrue( solver.solve( new int[]{3,5,7,9,11}, 8 ));
+		// Lukt niet
+		//assertFalse( solver.solve( new int[]{2,4}, 5 ));
+		// E�n te weinig
+		//assertFalse( solver.solve( new int[]{1,1,2,2,3,3,4,4,5,5}, 31 ) );
+		// Precies goed
+		//assertTrue( solver.solve( new int[]{1,1,1,2,2,3,3,4,4,5,5}, 31 ) );
+	}
+
+
+}
