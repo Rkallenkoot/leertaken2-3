0c95a33a40d52221654a25506d7fd6e01324f9fb
Roelof Kallenkoot <roelof000@gmail.com>
2016-03-08 03:28:48 +0100

    Denk dat die het doet

--- leertaak3-6/src/ttt/TicTacToe.java
+++ leertaak3-6/src/ttt/TicTacToe.java
@@ -52,33 +52,58 @@ public class TicTacToe
 	}
 
 	public int chooseMove() {
-	    //Best best=chooseMove(COMPUTER);
-	    //return best.row*3+best.column;
-	    return 0;
+	    Best best = chooseMove(COMPUTER);
+	    return best.row*3+best.column;
     }
     
     // Find optimal move
 	private Best chooseMove(int side) {
-		int opp;              // The other side
+		int opp = side == COMPUTER ? HUMAN : COMPUTER; // The other side
 		Best reply;           // Opponent's best reply
 		int simpleEval;       // Result of an immediate evaluation
+        int checkBoard[][] = board.clone();
 		int bestRow = 0;
 		int bestColumn = 0;
-		int value;
+
+		int value = side  == COMPUTER ? COMPUTER_WIN : HUMAN_WIN;
 
 		if((simpleEval = positionValue()) != UNCLEAR) {
             return new Best(simpleEval);
         }
 
-		// TODO: implementeren m.b.v. recursie/backtracking
-	    return null;
+        for (int y = 0; y < checkBoard.length; y++) {
+            for (int x = 0; x < checkBoard[1].length; x++) {
+                if(!squareIsEmpty(x, y) || !moveOk(x,y)){
+                    // Continue if square is not not empty or move not Ok
+                    // moveOk currently does the same as square not empty**
+                    continue;
+                }
+                place(x,y, side);
+                reply = chooseMove(opp);
+                if(reply.val < value){
+                    value = reply.val;
+                    bestRow = x;
+                    bestColumn = y;
+                }
+                place(x, y, EMPTY);
+            }
+        }
+
+
+	    return new Best(value, bestRow, bestColumn);
     }
 
    
     //check if move ok
     public boolean moveOk(int move) {
  	    return (move >= 0 && move <=8 && board[move / 3][move % 3] == EMPTY);
+    }
 
+    public boolean moveOk(int x, int y){
+        if(x < 0 || x > 2 || y < 0 || y > 2){
+            return false;
+        }
+        return squareIsEmpty(x,y);
     }
     
     // play move
@@ -94,8 +119,7 @@ public class TicTacToe
 
 
 	// Simple supporting routines
-	private void clearBoard( )
-	{
+	private void clearBoard( ) {
 		for (int x = 0; x < board.length; x++) {
 			for (int y = 0; y < board.length; y++) {
 				board[y][x] = EMPTY;
@@ -104,17 +128,45 @@ public class TicTacToe
 	}
 
 
-	private boolean boardIsFull( )
-	{
-		//TODO:
+	private boolean boardIsFull() {
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board.length; x++) {
+                int cell = board[x][y];
+                if(cell == EMPTY){
+                    return false;
+                }
+            }
+        }
+
 		return true;
 	}
 
 	// Returns whether 'side' has won in this position
-	private boolean isAWin(int side)
-	{
-	    //TODO:
-	    return true;
+	private boolean isAWin(int side) {
+        // all winning positions
+        int [][] winningPositions = {
+                { 0, 1, 2 },
+                { 3, 4, 5 },
+                { 6, 7, 8 },
+
+                { 0, 3, 6 },
+                { 1, 4, 7 },
+                { 2, 5, 8 },
+
+                { 0, 4, 8 },
+                { 2, 4, 6 },
+        };
+
+        for (int[] winningPosition : winningPositions) {
+            // if every position in this winning position is from the same Side, then Side has won
+            if (getPlace(winningPosition[0]) == side &&
+                    getPlace(winningPosition[1]) == side &&
+                    getPlace(winningPosition[2]) == side) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 	// Play a move, possibly clearing a square
@@ -123,7 +175,12 @@ public class TicTacToe
 		board[row][column] = piece;
 	}
 
-	private boolean squareIsEmpty( int row, int column )
+    private int getPlace(int move){
+        return board[move /3][move % 3];
+    }
+
+    // TODO: 3/8/2016 out of bound array exception? - Roelof
+    private boolean squareIsEmpty( int row, int column )
 	{
 		return board[row][column] == EMPTY;
 	}
@@ -131,15 +188,39 @@ public class TicTacToe
 	// Compute static value of current position (win, draw, etc.)
 	private int positionValue( )
 	{
-		// TODO:
-		return UNCLEAR;
+
+        if(isAWin(HUMAN)) {
+            return HUMAN_WIN;
+        }
+        else if(isAWin(COMPUTER)) {
+            return COMPUTER_WIN;
+        }
+        else if(boardIsFull()) {
+            return DRAW;
+        }
+
+        return UNCLEAR;
 	}
 	
 	
 	public String toString()
 	{
-	    //TODO:
-		return "...\n...\n...\n";
+        StringBuilder output = new StringBuilder();
+
+        for (int x = 0; x < board[0].length; x++) {
+            for (int y = 0; y < board[1].length; y++) {
+                int cell = board[x][y];
+                if(cell == HUMAN){
+                    output.append(humanChar);
+                } else if (cell == COMPUTER){
+                    output.append(computerChar);
+                } else {
+                    output.append('.');
+                }
+            }
+            output.append('\n');
+        }
+		return output.toString();
 	}  
 	
 	public boolean gameOver() {
@@ -148,9 +229,15 @@ public class TicTacToe
     }
     
     public String winner() {
-        if      (this.position==COMPUTER_WIN) return "computer";
-        else if (this.position==HUMAN_WIN   ) return "human";
-        else                                  return "nobody";
+        if(this.position==COMPUTER_WIN){
+            return "computer";
+        }
+        else if(this.position==HUMAN_WIN){
+            return "human";
+        }
+        else{
+            return "nobody";
+        }
     }
 
 
@@ -160,10 +247,10 @@ public class TicTacToe
         int val;
 
         public Best( int v ){
-            this( v, 0, 0 );
+            this(v, 0, 0);
         }
 
-        public Best( int v, int r, int c ){
+        public Best(int v, int r, int c){
             val = v; row = r; column = c;
         }
     }
